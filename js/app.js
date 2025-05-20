1efa894eee8397bb5fcd80db99c957670fbda51c
Joao Abe <joaovictorsouzaabe@gmail.com>
2025-05-20 14:50:31 -0300

    new-filter

--- js/app.js
+++ js/app.js
@@ -3,6 +3,8 @@ const btnListar = document.getElementById("btnListar")
 const selectCategorias = document.getElementById("selectCategorias")
 const chkDisponiveis = document.getElementById("chkDisponiveis")
 const btnFiltrar = document.getElementById("btnFiltrar")
+const btnLimpar = document.getElementById("btnLimpar")
+const listaProdutos = document.getElementById("listaProdutos"); 
 
 
 const produtos = [
@@ -18,4 +20,61 @@ const produtos = [
     {nome: "Borracha", preco: 50, categoria: "borracha", disponibilidade: true}
 ];
 
-console.log(produtos)
+
+function exibirProdutos(produtosFiltrados) {
+    const erro = document.getElementById("erro");
+    erro.textContent = ""; // limpa mensagens anteriores
+    listaProdutos.innerHTML = ""; // limpa lista antes de exibir
+
+    if (produtosFiltrados.length === 0) {
+        erro.textContent = "Nenhum produto encontrado.";
+        return;
+    }
+
+    for (var i = 0; i < produtosFiltrados.length; i++) {
+        var p = produtosFiltrados[i];
+
+        let cardProduto = document.createElement("div");
+        cardProduto.innerHTML = `
+            <h3>Nome: ${p.nome}</h3>
+            <p>Preço: R$ ${p.preco}</p>
+            <p>Categoria: ${p.categoria}</p>
+            <p>Disponibilidade: ${p.disponibilidade ? "Sim" : "Não"}</p>
+        `;
+        listaProdutos.appendChild(cardProduto);
+    }
+    form.reset();
+}
+
+
+btnListar.addEventListener("click", function(event){
+    event.preventDefault();
+    exibirProdutos(produtos);
+})
+
+btnFiltrar.addEventListener("click", function(event){
+    event.preventDefault();
+    
+    var selecionado = selectCategorias.value;
+    var somenteDisponiveis = chkDisponiveis.checked;
+
+    var filtrados = produtos.filter(function(p){
+        return p.categoria === selecionado;
+    });
+
+    if (somenteDisponiveis) {
+        filtrados = filtrados.filter(function(p){
+            console.log(p.disponibilidade)
+            return p.disponibilidade;
+        });
+    }
+
+    exibirProdutos(filtrados);
+});
+
+btnLimpar.addEventListener("click" , function(event){
+    event.preventDefault();
+    listaProdutos.innerHTML = "";
+    erro.textContent = "";
+})
+
